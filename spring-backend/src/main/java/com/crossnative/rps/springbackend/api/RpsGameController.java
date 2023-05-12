69f15344a8c649151820527ecab204cefc494eb3
Jan Stamer <jan.stamer@ppi.de>
2023-05-12 16:56:56 +0200

    Set CORS headers.

--- spring-backend/src/main/java/com/crossnative/rps/springbackend/api/RpsGameController.java
+++ spring-backend/src/main/java/com/crossnative/rps/springbackend/api/RpsGameController.java
@@ -5,6 +5,8 @@ import com.crossnative.rps.springbackend.model.Player;
 import com.crossnative.rps.springbackend.service.RpsGameService;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/v1/games")
