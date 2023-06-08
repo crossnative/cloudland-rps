27b279924638a6faa28275372e228d359c118dd3
Lena Grimm <lena.grimm@ppi.de>
2023-06-08 18:46:21 +0200

    logging in spring backend

--- spring-backend/src/main/java/com/crossnative/rps/springbackend/service/RpsGameService.java
+++ spring-backend/src/main/java/com/crossnative/rps/springbackend/service/RpsGameService.java
@@ -8,57 +8,16 @@ import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class RpsGameService implements GameService {
 
   private static final Random RANDOM = new Random();
 
   private final Map<UUID, Game> rpsGames = new HashMap<>();
-
-  @AllArgsConstructor
-  private enum Choice {
-    ROCK, PAPER, SCISSORS;
-
-    public static int fromPlayer(final Player player) {
-      return Choice.valueOf(player.getChoice().toUpperCase()).ordinal();
-    }
-  }
-
-  @Override
-  public final Game getGame(final UUID id) {
-    return this.rpsGames.get(id);
-  }
-
-  @Override
-  public Game createGame() {
-    final var game = new Game(RpsGameService::evaluate);
-    this.rpsGames.put(game.getId(), game);
-    return game;
-  }
-
-  @Override
-  public Game joinGame(final UUID gameId, final Player player) {
-    final var game = this.rpsGames.get(gameId);
-    game.addPlayer(player);
-    return game;
-  }
-
-  @Override
-  public void pickRandomChoice(Player player) {
-    var choices = Choice.values();
-    player.setChoice(choices[RANDOM.nextInt(choices.length)].toString());
-  }
-
-  @Override
-  public Game registerPlayerChoice(final UUID gameId, final UUID playerId,
-      final String choice) {
-    final var game = this.rpsGames.get(gameId);
-    final var choosingPlayer = game.getPlayer(playerId);
-    choosingPlayer.setChoice(Choice.valueOf(choice.toUpperCase()).name());
-    return game;
-  }
 
   private static GameResult evaluate(final Player player1, final Player player2) {
     final var result = Math.floorMod(Choice.fromPlayer(player1)
@@ -70,7 +29,54 @@ public class RpsGameService implements GameService {
       case 2 -> GameResult.playerWins(player2, player1);
       default -> null;
     };
+  }
 
+  @Override
+  public final Game getGame(final UUID id) {
+    log.debug("Getting game...");
+    return this.rpsGames.get(id);
+  }
+
+  @Override
+  public Game createGame() {
+    log.debug("Creating game...");
+    final var game = new Game(RpsGameService::evaluate);
+    this.rpsGames.put(game.getId(), game);
+    return game;
+  }
+
+  @Override
+  public Game joinGame(final UUID gameId, final Player player) {
+    log.debug("Joining game...");
+    final var game = this.rpsGames.get(gameId);
+    game.addPlayer(player);
+    return game;
+  }
+
+  @Override
+  public void pickRandomChoice(Player player) {
+    log.debug("Picking a random game choice...");
+    var choices = Choice.values();
+    player.setChoice(choices[RANDOM.nextInt(choices.length)].toString());
+  }
+
+  @Override
+  public Game registerPlayerChoice(final UUID gameId, final UUID playerId,
+      final String choice) {
+    log.debug("Registering player choice...");
+    final var game = this.rpsGames.get(gameId);
+    final var choosingPlayer = game.getPlayer(playerId);
+    choosingPlayer.setChoice(Choice.valueOf(choice.toUpperCase()).name());
+    return game;
+  }
+
+  @AllArgsConstructor
+  private enum Choice {
+    ROCK, PAPER, SCISSORS;
+
+    public static int fromPlayer(final Player player) {
+      return Choice.valueOf(player.getChoice().toUpperCase()).ordinal();
+    }
   }
 
 }
