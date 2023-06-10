2cc5da954222b50e6883a9245890507fc2ea6608
Jan Stamer <jan.stamer@ppi.de>
2023-06-10 11:47:05 +0200

    Create initial state for go session.

--- backend-go/main.go
+++ backend-go/main.go
@@ -1,26 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/kelseyhightower/envconfig"
 	"github.com/rs/cors"
 )
-
-// Application Configuration
-type Config struct {
-	Port string `envconfig:"PORT" default:"8080"`
-}
 
 func main() {
 	r := chi.NewRouter()
 
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
 	r.Use(cors.AllowAll().Handler)
 
 	gameRepository := NewInMemoryGameRepository()
@@ -28,22 +17,14 @@ func main() {
 	r.Route("/api/v1", func(r chi.Router) {
 		r.Post("/games", createGameHandler(gameRepository))
 		r.Get("/games/{GameID}", readGameHandler(gameRepository))
-		r.Post("/games/{GameID}/players", joinGameHandler(gameRepository))
-		r.Patch("/games/{GameID}/players/{PlayerID}", playerChooseHandler(gameRepository))
+
+		// r.Post("/games/{GameID}/players", joinGameHandler(gameRepository))
+		// r.Patch("/games/{GameID}/players/{PlayerID}", playerChooseHandler(gameRepository))
 	})
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello Cloudland!"))
+		w.Write([]byte("Hello CloudLand!"))
 	})
 
-	// Read Configuration from Environment Variables
-	var c Config
-	err := envconfig.Process("ros", &c)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	log.Println("listening on", c.Port)
-
-	http.ListenAndServe(fmt.Sprintf(":%v", c.Port), r)
+	http.ListenAndServe(":8080", r)
 }
